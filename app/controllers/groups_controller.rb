26c2b82734bea40a0a95d67eb1481977cc16f287
ayacai115 <countrylife.scone@gmail.com>
2017-05-26 20:22:16 +0900

    Add edit_group function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -16,7 +16,15 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
+
+  def update
+    @group = Group.find(params[:id])
+    @group.update(create_params)
+    redirect_to root_path
+  end
+
 
   private
   def create_params
