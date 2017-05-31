c2f217a328e91a721d199c8678eb7b8c4715b9c0
ayacai115 <countrylife.scone@gmail.com>
2017-05-31 14:31:03 +0900

    Add sidebar link

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,12 @@
 class MessagesController < ApplicationController
+  def index
+    redirect_to_groups_show
+  end
+
   def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to group_path(params[:group_id])
+      redirect_to_groups_show
     else
       redirect_to group_path(params[:group_id]), flash: { alert: "メッセージを入力してください。" }
     end
@@ -12,5 +16,9 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  def redirect_to_groups_show
+    redirect_to group_path(params[:group_id])
   end
 end
