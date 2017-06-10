1435103a70b1da15a314a6dd6197e1f16e36d990
ayacai115 <countrylife.scone@gmail.com>
2017-06-10 17:30:31 +0900

    Refactored some files

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -44,7 +44,6 @@ $(function() {
         messagesList.append(html);
       });
       messagesList.animate({scrollTop: messagesList[0].scrollHeight}, 0);
-      $("body").animate({scrollTop: $(document).height()}, 0);
     })
     .fail(function() {
       alert('問題が発生しました');
